69b0dbbacfb3be70362815f20f9302e67ecefde8
ashdkx <ashdkx@yahoo.com>
2024-09-11 12:48:38 -0400

    completed the modified game with rotating, TODO: animation

--- src/Game.js
+++ src/Game.js
@@ -5,15 +5,16 @@ function Square({value, onSquareClick}) {
     return <button className="square" onClick={onSquareClick}> {value}</button>
 }
 
-function RotBtn({value, onRotLeftClick}) {
-    return <button className="round" onClick={onRotLeftClick}> {value} </button>
+function RotBtn({value, onRotClick}) {
+    return <button className="round" onClick={onRotClick}> {value} </button>
 }
 
 
 export default function Board() {
     const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [squares, setSquares] = useState(Array(16).fill(null));
     const winner = calculateWinner(squares);
+    const [isMove, setIsMove] = useState(true);
     let status;
     if (winner) {
         status = "Winner: " + winner;
@@ -21,9 +22,12 @@ export default function Board() {
         status = "Next player: " + (xIsNext ? "X": "O");
     }
 
-    function handleClick(i) {
+    function handleSquareClick(i) {
         const nextSquare = squares.slice();
-        if (nextSquare[i] || calculateWinner(squares)) {
+        if (!isMove) {
+            return
+        }
+        if (nextSquare[i] || winner) {
             return
         }
         if (xIsNext) {
@@ -33,43 +37,79 @@ export default function Board() {
         }
         setSquares(nextSquare);
         setXIsNext(!xIsNext);
+        setIsMove(false);
     }
+
+    // Rotate right
+    // 0  1  2  3       4  0  1  2
+    // 4  5  6  7    => 8  9  5  3
+    // 8  9  10 11      12 10 6  7
+    // 12 13 14 15      13 14 15 11
+
+    function handleRotRightClick() {
+        if (isMove || winner) {
+            return
+        }
+        const rotationMap = [4, 0, 1, 2, 8, 9, 5, 3, 12, 10, 6, 7, 13, 14, 15, 11];
+        const nextSquare = rotationMap.map(index => squares[index]);
+
+        calculateWinner(nextSquare);
+        setSquares(nextSquare);
+        setIsMove(true);
+    }
+
+    // Rotate left
+    // 0  1  2  3       1  2  3  7
+    // 4  5  6  7    => 0  6  10 11
+    // 8  9  10 11      4  5  9  15
+    // 12 13 14 15      8  12 13 14
+
+    function handleRotLeftClick() {
+        if (isMove || winner) {
+            return
+        }
+        const rotationMap = [1, 2, 3, 7, 0, 6, 10, 11, 4, 5, 9, 15, 8, 12, 13, 14];
+        const nextSquare = rotationMap.map(index => squares[index]);
+
+        calculateWinner(nextSquare);
+        setSquares(nextSquare);
+        setIsMove(true);
+    }
+
 
     return (
         <>
             <div className="status"> {status} </div>
             <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)}/>
+                <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)}/>
+                <Square value={squares[2]} onSquareClick={() => handleSquareClick(2)}/>
+                <Square value={squares[3]} onSquareClick={() => handleSquareClick(3)}/>
 
             </div>
             <div className="board-row">
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                <Square value={squares[4]} onSquareClick={() => handleSquareClick(4)}/>
+                <Square value={squares[5]} onSquareClick={() => handleSquareClick(5)}/>
+                <Square value={squares[6]} onSquareClick={() => handleSquareClick(6)}/>
+                <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)}/>
 
             </div>
             <div className="board-row">
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-                <Square value={squares[9]} onSquareClick={() => handleClick(9)}/>
-                <Square value={squares[10]} onSquareClick={() => handleClick(10)}/>
-                <Square value={squares[11]} onSquareClick={() => handleClick(11)}/>
+                <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)}/>
+                <Square value={squares[9]} onSquareClick={() => handleSquareClick(9)}/>
+                <Square value={squares[10]} onSquareClick={() => handleSquareClick(10)}/>
+                <Square value={squares[11]} onSquareClick={() => handleSquareClick(11)}/>
             </div>
             <div className="board-row">
-                <Square value={squares[12]} onSquareClick={() => handleClick(12)}/>
-                <Square value={squares[13]} onSquareClick={() => handleClick(13)}/>
-                <Square value={squares[14]} onSquareClick={() => handleClick(14)}/>
-                <Square value={squares[15]} onSquareClick={() => handleClick(15)}/>
+                <Square value={squares[12]} onSquareClick={() => handleSquareClick(12)}/>
+                <Square value={squares[13]} onSquareClick={() => handleSquareClick(13)}/>
+                <Square value={squares[14]} onSquareClick={() => handleSquareClick(14)}/>
+                <Square value={squares[15]} onSquareClick={() => handleSquareClick(15)}/>
             </div>
 
             <div className="div-btn">
-                <RotBtn value="<" onRotLeftClick={() => {
-                }}/>
-                <RotBtn value=">" onRotLeftClick={() => {
-                }}/>
+                <RotBtn value="<" onRotClick={() => handleRotLeftClick()}/>
+                <RotBtn value=">" onRotClick={() => handleRotRightClick()}/>
             </div>
         </>
 
@@ -89,8 +129,8 @@ function calculateWinner(squares) {
         [3, 6, 9, 12]
     ];
     for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        const [a, b, c, d] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
             return squares[a];
         }
     }
